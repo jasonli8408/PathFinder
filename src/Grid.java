7fed26c97869d6d5be5aff5810a21f6a00f36f69
Kian <kianhk6@gmail.com>
2021-12-28 16:46:00 -0800

    updated version 2.0 by kian

--- src/Grid.java
+++ src/Grid.java
@@ -3,12 +3,10 @@ public class Grid {
     public Grid(int rows, int cols) {
         grid = new Node[rows][cols];
         for(int i = 0; i < rows; i++){
-
+            for(int j = 0; j < cols; j++){
+                Node temp = new Node(i,j);
+                grid[i][j] = temp;
+            }
         }
     }
-
-
-
-
-
 }
