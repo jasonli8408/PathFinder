b7c96a7384ae9b726ca0f1959f367c5be5f6c393
alexk <alexkang12345@outlook.com>
2021-12-31 14:24:18 -0800

    grid commit again

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -6,20 +7,130 @@ public class GUI extends JPanel implements MouseWheelListener, MouseListener, Ke
 
     private JFrame window;
     private PathFinder pathFinder;
-    private final static int gridDimention = 25;
+    private final static int gridDimention = 30;
+    private Character keyRightNow;
+    private Node startNode;
+    private Node endNode;
 
 
 
-    public GUI(){
+
+    public GUI() {
+        keyRightNow = 'a';
+        startNode = null;
+        endNode = null;
         window = new JFrame();
         window.setContentPane(this);
-        window.getContentPane().setPreferredSize(new Dimension(1000,1000));
+        window.getContentPane().setPreferredSize(new Dimension(900,900));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //how it would close
         window.setTitle("A* algorithm");
         window.pack();
         window.setVisible(true);
         window.repaint();
     }
+
+
+    {
+        JFrame frame = new JFrame();
+        frame.setTitle("Control Panel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Font size and style
+        Font font = new Font("Verdana", Font.BOLD, 30);
+
+        JButton chooseStart, chooseEnd, chooseObstacles, findPath, incrementGridSize, decrementGridSize;
+
+        class RoundedBorder implements Border {
+            private final int radius;
+
+            RoundedBorder(int radius) {
+                this.radius = radius;
+            }
+
+            public Insets getBorderInsets(Component c) {
+                return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+            }
+
+            public boolean isBorderOpaque() {
+                return true;
+            }
+
+            public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+            }
+        }
+
+        chooseStart = new JButton("Choose Start Node");
+        chooseStart.setSize(new Dimension(10, 40));
+        chooseStart.setBorder(new RoundedBorder(10));
+        chooseStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        chooseEnd = new JButton("Choose End Node");
+        chooseEnd.setSize(new Dimension(10, 40));
+        chooseEnd.setBorder(new RoundedBorder(10));
+        chooseEnd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        chooseObstacles = new JButton("Create Obstacles");
+        chooseObstacles.setSize(new Dimension(10, 40));
+        chooseObstacles.setBorder(new RoundedBorder(10));
+        chooseObstacles.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        findPath = new JButton("A* Find Path");
+        findPath.setSize(new Dimension(10, 40));
+        findPath.setBorder(new RoundedBorder(10));
+        findPath.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        incrementGridSize = new JButton("Increment Grid Size");
+        incrementGridSize.setSize(new Dimension(10, 40));
+        incrementGridSize.setBorder(new RoundedBorder(10));
+        incrementGridSize.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        decrementGridSize = new JButton("Decrement Grid Size");
+        decrementGridSize.setSize(new Dimension(10, 40));
+        decrementGridSize.setBorder(new RoundedBorder(10));
+        decrementGridSize.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridLayout(6,1));
+        p2.add(chooseStart);
+        p2.add(chooseEnd);
+        p2.add(chooseObstacles);
+        p2.add(findPath);
+        p2.add(incrementGridSize);
+        p2.add(decrementGridSize);
+
+
+        frame.setLayout(new BorderLayout());
+        frame.add(p2,BorderLayout.LINE_END);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
 
     @Override
     public void paintComponent(Graphics g) {
@@ -32,6 +143,11 @@ public class GUI extends JPanel implements MouseWheelListener, MouseListener, Ke
             for (int x = 0; x < this.getWidth(); x += gridDimention) {
                 g.drawRect(x, y , gridDimention, gridDimention);
             }
+        }
+
+
+        if (endNode != null) {
+            g.fillRect(endNode.getX(), endNode.getY(), gridDimention, gridDimention);
         }
     }
 
@@ -47,7 +163,8 @@ public class GUI extends JPanel implements MouseWheelListener, MouseListener, Ke
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        char key = e.getKeyChar();
+        keyRightNow = key;
     }
 
     @Override
@@ -57,6 +174,19 @@ public class GUI extends JPanel implements MouseWheelListener, MouseListener, Ke
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        if (keyRightNow == 'e') {
+            int x = e.getX();
+            int y = e.getY();
+
+            if (endNode == null) {
+                //create an end node
+                startNode = new Node(x, y);
+            }
+
+
+        }
+
+        repaint();
 
 
     }
