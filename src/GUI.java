17e0d77229a0999c67b672133bc5b8ecd5336d74
alexk <alexkang12345@outlook.com>
2021-12-31 13:12:51 -0800

    Window commit

--- src/GUI.java
+++ src/GUI.java
@@ -4,17 +4,18 @@ import java.awt.event.*;
 
 public class GUI extends JPanel implements MouseWheelListener, MouseListener, KeyListener, ActionListener {
 
+    private JFrame window;
+
 
     public GUI(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(40 ,40));
-        frame.add(panel, BorderLayout.CENTER); // adds the pannel
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //how it would close
-        frame.setTitle("A* algorithm");
-        frame.pack();
-        frame.setVisible(true);
+        window = new JFrame();
+        window.setContentPane(this);
+        window.getContentPane().setPreferredSize(new Dimension(1000,1000));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //how it would close
+        window.setTitle("A* algorithm");
+        window.pack();
+        window.setVisible(true);
+        window.repaint();
     }
 
     @Override
