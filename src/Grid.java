8c0091ed6785d13fd7543e6467c07027a4c2b52c
Kian <kianhk6@gmail.com>
2022-01-03 20:39:28 -0800

    Kian commit comments

--- src/Grid.java
+++ src/Grid.java
@@ -1,39 +1,39 @@
+//the class that represents the grid for the back-end
 public class Grid {
-    private final Node[][] grid;
-    private final int rows;
+    //this grid is used in pathFinder not GUI
+    private final Node[][] grid; // an array representing the grid
+    private final int rows; //number of columns and rows we have in our grid for pathfinder file
     private final int cols;
     public Grid(int rows, int cols) {
+        //it constructs the grid according the input of rows and columns
         this.rows = rows;
         this.cols = cols;
         grid = new Node[rows][cols];
         for(int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                Node temp = new Node(j, i);
-                grid[j][i] = temp;
+                Node temp = new Node(i, j);
+                grid[i][j] = temp;
             }
         }
     }
-
+    //basic getters
     public int getRows(){
         return this.rows;
     }
     public int getCols(){
         return this.cols;
     }
-
     public Node getNode(int rows, int column){
         return grid[rows][column];
     }
-
+    //these functions are also used in the GUI:
+    //if the node is a block according to its rows and columns
+    // we "flip" the node making it a block or if its already block we bring it back
     public void flipNode(int rows, int column){
         grid[rows][column].flipNode();
     }
-
+    //we check if the node is a block
     public boolean isValidNode(int rows, int column){
-
-            return grid[rows][column].isNode();
-        }
-
-
-
+        return grid[rows][column].isNode();
+    }
 }
